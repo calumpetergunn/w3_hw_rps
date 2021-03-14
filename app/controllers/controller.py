fdc773027eaaeb800236b1bbf2570b10bc980e2b
calumpetergunn <calumgunn@gmail.com>
2021-03-14 10:48:41 +0000

    added welcome page

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,8 +1,23 @@
 from flask import render_template
 from app import app
-from app.models.game import game
+from app.models.game import find_winner
 from app.models.player import Player
 
 @app.route('/game')
 def index():
-    return "Let's play a game!"
+    return render_template("index.html")
+
+@app.route('/<choice1>/<choice2>')
+def play_game(choice1, choice2):
+    player1 = Player("Little Richard", choice1)
+    player2 = Player("Glenn Danzig", choice2)
+    get_result = find_winner(player1, player2)
+    return render_template('index.html', result=get_result)
+
+@app.route('/welcome')
+def welcome():
+    return render_template("welcome.html")
+    
+
+    
+
--- app/models/game.py
+++ app/models/game.py
@@ -1,7 +1,22 @@
-from app.models.player import *
+def find_winner(player1, player2):
+    if player1.player_choice == player2.player_choice:
+        return "It's a draw!"
+    elif player1.player_choice == "rock":
+        if player2.player_choice == "scissors":
+            return player1.player_name + " wins with " + player1.player_choice
+        else:
+            return player2.player_name + " wins with " + player2.player_choice
+    elif player1.player_choice == "paper":
+        if player2.player_choice == "rock":
+            return player1.player_name + " wins with " + player1.player_choice
+        else:
+            return player2.player_name + " wins with " + player2.player_choice
+    elif player1.player_choice == "scissors":
+        if player2.player_choice == "paper":
+            return player1.player_name + " wins with " + player1.player_choice
+        else:
+            return player2.player_name + " wins with " + player2.player_choice
 
+    
 
-player1 = Player("Little Richard", "Rock")
-player2 = Player("Glenn Danzig", "Scissors")
-
-game = [player1, player2]
+   
