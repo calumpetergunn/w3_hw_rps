839eca6d264a9030d9e9ce3498db74737ac48d8a
calumpetergunn <calumgunn@gmail.com>
2021-03-15 08:44:12 +0000

    added game class

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect
 from app import app
-from app.models.game import find_winner, computer_result
+from app.models.game import find_winner, computer_result, Game
 from app.models.player import Player
 
 @app.route('/game')
--- app/models/game.py
+++ app/models/game.py
@@ -1,5 +1,10 @@
 import random
 
+class Game():
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
+
 def find_winner(player1, player2):
     if player1.player_choice == player2.player_choice:
         return "It's a draw!"
